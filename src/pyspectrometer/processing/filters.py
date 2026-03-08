9d48db1c461a529b78ec79f8599729a423cc395e
FoxIS <andrius.mikonis@gmail.com>
2026-03-08 14:51:03 +0200

    fix: old npy.

--- src/pyspectrometer/processing/filters.py
+++ src/pyspectrometer/processing/filters.py
@@ -50,7 +50,7 @@ def savitzky_golay(
     order_range = range(order + 1)
     half_window = (window_size - 1) // 2
     
-    b = np.mat([
+    b = np.asmatrix([
         [k ** i for i in order_range]
         for k in range(-half_window, half_window + 1)
     ])
